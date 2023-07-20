4adc8b913772330f61ab1fa8f7ce8d39663a940b
sergio-rocha1 <sergio.rocha.borges@gmail.com>
2023-07-19 23:50:11 -0300

    Alterado o pom.xml adicionando as dependencias do Spring Security; Adicionado migration referente criação da tabela de usuários no banco de dados; Adicionado a entidade JPA "Usuarios" e sua classe Repository; Adicionado classe "AutenticacaoService" para realizar a autenticação da API com o Spring Security.

---
+++ src/main/java/br/com/med/cia/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.med.cia.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
